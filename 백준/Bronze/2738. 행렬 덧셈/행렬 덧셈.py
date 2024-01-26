53b67f0b61b2f3a35188c5cba13b1723f0a84967
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2024-01-27 03:31:47 +0900

    Update 행렬 덧셈.py

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -5,7 +5,7 @@ N, M = map(int, sys.stdin.readline().rstrip().split())
 A = [list(map(int, sys.stdin.readline().rstrip().split())) for _ in range(N)]
 
 for i in range(N):
-    data = [int(x) for x in input().split()]
+    data = [int(x) for x in sys.stdin.readline().rstrip().split()]
     for j in range(M):
         A[i][j] += data[j]
 
