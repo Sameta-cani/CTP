2b93c9649a8b72974c710c800113b3106499026f
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2024-01-28 01:39:55 +0900

    [Bronze III] Title: 최댓값, Time: 52 ms, Memory: 39640 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.py
+++ 백준/Bronze/2566. 최댓값/최댓값.py
@@ -1,17 +1,17 @@
 import random
+import sys
 
 max_value = float('-inf')
-max_index = list()
+max_positions = list()
 
 for i in range(1, 10):
-    data = list(map(int, input().split()))
+    data = list(map(int, sys.stdin.readline().rstrip().split()))
     for j, val in enumerate(data):
         if val > max_value:
-            if data == max_value:
-                max_index.append((i, j+1))
-            else:
-                max_index = [(i, j+1)]
-                max_value = val
+            max_positions = [(i, j+1)]
+            max_value = val
+        elif val == max_value:
+            max_positions.append((i, j+1))
 
 print(max_value)
-print(*random.choice(max_index))
+print(*random.choice(max_positions))
