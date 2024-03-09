57f26314cb5ed02c51dd90c7268f596dc522e1ab
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2024-03-10 01:10:41 +0900

    [Bronze I] Title: 알고리즘 수업 - 선택 정렬 1, Time: 1864 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Bronze/23881. 알고리즘 수업 － 선택 정렬 1/알고리즘 수업 － 선택 정렬 1.py
+++ 백준/Bronze/23881. 알고리즘 수업 － 선택 정렬 1/알고리즘 수업 － 선택 정렬 1.py
@@ -1,3 +1,5 @@
+import sys
+
 def selection_sort(array, limit):
     count = 0
     for last in range(len(array)-1, -1, -1):
@@ -6,14 +8,14 @@ def selection_sort(array, limit):
             if array[i] > array[max_index]:
                 max_index = i
         if last != max_index:
-            array[last], array[max_index] = array[max_index], array[last]
             count += 1
-        if count == limit:
-            return array[last], array[max_index]
+            if count == limit:
+                return array[max_index], array[last]
+            array[last], array[max_index] = array[max_index], array[last]
     return -1
 
-N, K = map(int, input().split())
-array = list(map(int, input().split()))
+N, K = map(int, sys.stdin.readline().rstrip().split())
+array = list(map(int, sys.stdin.readline().rstrip().split()))
 answer = selection_sort(array, K)
 
 if type(answer) == int:
