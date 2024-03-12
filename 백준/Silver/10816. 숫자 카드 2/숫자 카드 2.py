bd58cae9bd7a51e1f48a99b247ce8c005a101235
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2024-03-12 16:34:38 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1472 ms, Memory: 113548 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,15 +1,17 @@
 from bisect import bisect_left, bisect_right
 import sys
 
+input = sys.stdin.readline
+
 def count_by_range(array, left_value, right_value):
     right_index = bisect_right(array, right_value)
     left_index = bisect_left(array, left_value)
     return right_index - left_index
 
-N = int(sys.stdin.readline().rstrip())
-array = sorted(list(map(int, sys.stdin.readline().rstrip().split())))
-M = int(sys.stdin.readline().rstrip())
+N = int(input().rstrip())
+array = sorted(list(map(int, input().rstrip().split())))
+M = int(input().rstrip())
 
 
-for val in list(map(int, sys.stdin.readline().rstrip().split())):
+for val in list(map(int, input().rstrip().split())):
     print(count_by_range(array, val, val), end=' ')
