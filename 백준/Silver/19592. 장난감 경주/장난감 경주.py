83444d177845fa48cc55a113da170357aa490202
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2024-03-14 01:00:53 +0900

    Update 장난감 경주.py

--- 백준/Silver/19592. 장난감 경주/장난감 경주.py
+++ 백준/Silver/19592. 장난감 경주/장난감 경주.py
@@ -4,27 +4,31 @@ for _ in range(T):
     N, X, Y = map(int, input().split())
     Vs = list(map(int, input().split()))
 
-    # base 보다 커야 단독 우승
+    # 단독 우승에 필요한 기준 시간 계산
     base_time = X / max(Vs)
 
-    start = 0
-    end = Y
-    result = -1
-
+    # 내 속도
     my_V = Vs[-1]
+
+    # 부스터를 안 써도 되는 경우: 이미 가장 빠르면 부스터 필요 없음
     if max(Vs[:-1]) < my_V:
         print(0)
     else:
+        start, end = 0, Y
+        result = -1
+        # 부스터를 써야 하는 경우 이분 탐색
         while start <= end:
+            # 부스터를 써서 갈 거리
             mid = (start + end) // 2
+            # 해당 거리를 갔을 때의 내 시간
             my_time = 1 + (X - mid) / Vs[N - 1]
-            if my_time == base_time:
-                result = mid + 1
-                break
-            elif my_time > base_time:
-                start = mid + 1
-                result = mid + 1
+            # 내 시간이 기준 시간보다 빠를 경우
+            if my_time < base_time:
+                result = mid # 부스터 사용으로 단독 우승 가능
+                end = mid - 1 # 더 적은 부스터 사용 거리를 찾기
+            # 내 시간이 기준 시간보다 느릴 경우
+            # 부스터로 가는 거리를 늘림
             else:
-                end = mid - 1
+                start = mid + 1 
 
-        print(-1 if result > Y else result)
+        print(result if result <= Y else -1) # 결과가 Y 이하인 경우에만 유효
