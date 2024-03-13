1382fc29b28d0e930408ccd909b6fa9c1efa2621
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2024-03-14 01:22:40 +0900

    [Silver IV] Title: 정수 제곱근, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2417. 정수 제곱근/정수 제곱근.py
@@ -0,0 +1,13 @@
+N = int(input())
+
+start = 0
+end = N
+
+while start <= end:
+    mid = (start + end) // 2
+    if mid ** 2 < N:
+        start = mid + 1
+    else:
+        end = mid - 1
+
+print(start)
