2270a7f431df54b7583d7edfe1cbe46ddd521cb8
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2024-03-31 18:59:42 +0900

    [Gold IV] Title: 키 순서, Time: 1384 ms, Memory: 112736 KB -BaekjoonHub

---
+++ 백준/Gold/2458. 키 순서/키 순서.py
@@ -0,0 +1,32 @@
+import sys
+
+input = sys.stdin.readline
+INF = int(1e9)
+
+N, M = map(int, input().split())
+graph = [[INF] * (N + 1) for _ in range(N + 1)]
+
+for a in range(1, N + 1):
+    for b in range(1, N + 1):
+        if a == b:
+            graph[a][b] = 0
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a][b] = 1
+
+for k in range(1, N + 1):
+    for a in range(1, N + 1):
+        for b in range(1, N + 1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+
+count = 0
+for a in range(1, N + 1):
+    is_possible = True
+    for b in range(1, N + 1):
+        if graph[a][b] == INF and graph[b][a] == INF:
+            is_possible = False
+            break
+    count += is_possible
+
+print(count)
