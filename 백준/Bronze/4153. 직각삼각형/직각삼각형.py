576d0396c54eb8879d62ef85334e50ea8b93e09c
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2024-04-05 00:44:28 +0900

    [Bronze III] Title: 직각삼각형, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -0,0 +1,9 @@
+while True:
+    data = sorted(list(map(int, input().split())))
+    if sum(data) == 0:
+        break
+
+    if data[0]**2 + data[1]**2 == data[2]**2:
+        print('right')
+    else:
+        print('wrong')
