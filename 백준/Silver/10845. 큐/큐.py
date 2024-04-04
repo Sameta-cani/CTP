ae4781d725f66de142a1e539f928bf2bfd2f25ab
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2024-04-05 01:15:01 +0900

    [Silver IV] Title: 큐, Time: 80 ms, Memory: 34160 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.py
+++ 백준/Silver/10845. 큐/큐.py
@@ -6,28 +6,17 @@ input = sys.stdin.readline
 queue = deque()
 
 for _ in range(int(input())):
-    oper = input().split()
+    command = input().split()
 
-    if len(oper) == 1:
-        oper = oper[0]
-        if oper == 'pop':
-            if not queue:
-                print(-1)
-            else:
-                print(queue.popleft())
-        elif oper == 'size':
-            print(len(queue))
-        elif oper == 'empty':
-            print(1 if not queue else 0)
-        elif oper == 'front':
-            if not queue:
-                print(-1)
-            else:
-                print(queue[0])
-        elif oper == 'back':
-            if not queue:
-                print(-1)
-            else:
-                print(queue[-1])
-    else:
-        queue.append(int(oper[1]))
+    if command[0] == 'pop':
+        print(queue.popleft() if queue else -1)
+    elif command[0] == 'size':
+        print(len(queue))
+    elif command[0] == 'empty':
+        print(0 if queue else 1)
+    elif command[0] == 'front':
+        print(queue[0] if queue else -1)
+    elif command[0] == 'back':
+        print(queue[-1] if queue else -1)
+    elif command[0] == 'push':
+        queue.append(int(command[1]))
