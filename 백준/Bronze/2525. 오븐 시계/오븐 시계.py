ac2bb3b9ef814f3cae50c16bd3b30dab302e61f5
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2024-04-05 00:31:18 +0900

    [Bronze III] Title: 오븐 시계, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.py
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -1,9 +1,4 @@
-import sys
+A, B = map(int, input().split())
+C = int(input())
 
-A, B = map(int, sys.stdin.readline().rstrip().split())
-C = int(sys.stdin.readline().rstrip())
-
-new_A = (A + (B + C) // 60) % 24
-new_B = (B + C) % 60
-
-print(new_A, new_B)
+print((A + ((B  + C) // 60)) % 24, (B + C) % 60)
