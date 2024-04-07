0d80bd2dda49bb8d4e3bbe0b954608db122b710f
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2024-04-07 19:37:50 +0900

    [Gold III] Title: ACM Craft, Time: 1344 ms, Memory: 34452 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.py
+++ 백준/Gold/1005. ACM Craft/ACM Craft.py
@@ -6,8 +6,7 @@ input = sys.stdin.readline
 
 for _ in range(int(input())):
     N, K = map(int, input().split())
-    Delays = list(map(int, input().split()))
-    cost = 0
+    Delays = [0] + list(map(int, input().split()))
 
     indegree = [0] * (N + 1)
 
@@ -24,15 +23,14 @@ for _ in range(int(input())):
     for i in range(1, N + 1):
         if indegree[i] == 0:
             q.append(i)
-            cost += Delays[i - 1]
 
     while q:
         now = q.popleft()
         for i in graph[now]:
-            result[i - 1] = max(result[i - 1], Delays[i - 1] + result[now - 1])
+            result[i] = max(result[i], Delays[i] + result[now])
             indegree[i] -= 1
             if indegree[i] == 0:
                 q.append(i)
 
     W = int(input())
-    print(result[W - 1])
+    print(result[W])
