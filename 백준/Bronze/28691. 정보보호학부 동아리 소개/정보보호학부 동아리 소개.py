7d9438f6e8cd1cb4cd78ff931a92d2fb06b55fcb
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2024-04-08 21:38:16 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.py
@@ -0,0 +1,12 @@
+C = input()
+
+if C == 'M':
+    print('MatKor')
+elif C == 'W':
+    print('WiCys')
+elif C == 'C':
+    print('CyKor')
+elif C == 'A':
+    print('AlKor')
+elif C == '$':
+    print('$clear')
