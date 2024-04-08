2ed0eb2d3c5560b8b448ffb18d9a80d430ac593d
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2024-04-08 21:25:29 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,10 @@
+Abb = input()
+
+if Abb == 'SONGDO':
+    print('HIGHSCHOOL')
+elif Abb == 'CODE':
+    print('MASTER')
+elif Abb == '2023':
+    print('0611')
+elif Abb == 'ALGORITHM':
+    print('CONTEST')
