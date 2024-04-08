9c3eb046c84204ea67a341ac175bb81d27ae6ec0
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2024-04-08 21:23:57 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,10 @@
+Abb = input()
+
+if Abb == 'NLCS':
+    print('North London Collegiate School')
+elif Abb == 'BHA':
+    print('Branksome Hall Asia')
+elif Abb == 'KIS':
+    print('Korea International School')
+elif Abb == 'SJA':
+    print('St. Johnsbury Academy')
