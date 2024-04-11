f2d3c472867f9eb52a7cb8e4f32a8c5875c98553
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2024-04-12 02:07:50 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 2848 ms, Memory: 34060 KB -BaekjoonHub

--- 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
@@ -10,25 +10,25 @@ robot = deque([False] * N)
 step = 1
 
 while True:
-    conveyor.appendleft(conveyor.pop())
-    robot.appendleft(robot.pop())
-    if robot[N - 1] == True:
-        robot[N - 1] = False
+    conveyor.rotate(1)
+    robot.rotate(1)
+    robot[-1] = False
 
     for i in range(N - 2, -1, -1):
-        if robot[i] == True and not robot[i + 1] and conveyor[i + 1] >= 1:
+        if robot[i] and not robot[i + 1] and conveyor[i + 1] > 0:
             conveyor[i + 1] -= 1
             robot[i + 1] = True
             robot[i] = False
 
     robot[-1] = False
-
+    
     if conveyor[0] > 0 and not robot[0]:
         conveyor[0] -= 1
         robot[0] = True
 
     if conveyor.count(0) >= K:
         break
+
     step += 1
 
 print(step)
