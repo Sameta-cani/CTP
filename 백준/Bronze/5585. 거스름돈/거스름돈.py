fbb54004baa210b50092f3b8fcddc285febd5a42
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2024-04-17 19:23:41 +0900

    [Bronze II] Title: 거스름돈, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/5585. 거스름돈/거스름돈.py
+++ 백준/Bronze/5585. 거스름돈/거스름돈.py
@@ -1,16 +1,12 @@
-# 사용할 수 있는 동전의 종료
-coin_types = [500, 100, 50, 10, 5, 1]
-# 사용할 동전의 수
+import sys
+
+input = sys.stdin.readline
+
 count = 0
+money = 1000 - int(input())
 
-# 거스름돈 계산 (1,000엔에서 입력받은 금액을 뺀 값)
-change = 1000 - int(input())
-
-# 각 동전에 대해
-for coin in coin_types:
-    # 해당 동전으로 거슬러 줄 수 있는 최대 개수를 더함
-    count += (change // coin)
-    # 거슬러 준 후 남은 금액
-    change %= coin
+for coin in [500, 100, 50, 10, 5, 1]:
+    count += money // coin
+    money %= coin
 
 print(count)
