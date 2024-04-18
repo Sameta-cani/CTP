b45d10f1f8cb9b3f14f8e01a7bf63ced74023b7d
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2024-04-18 19:11:18 +0900

    [Bronze I] Title: 컵홀더, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2810. 컵홀더/컵홀더.py
+++ 백준/Bronze/2810. 컵홀더/컵홀더.py
@@ -1,9 +1,11 @@
-# 좌석의 수 입력
-N = int(input())
+import sys
 
-# 좌석의 정보 입력
-seats = input()
+input = sys.stdin.readline
 
-# 커플석이 없다면 좌석 개수가 최대 사람의 수
-# 커플석이 있다면 'LL'을 'L'으로 치환하고 +1
-print(len(seats.replace('LL', 'L')) + 1 if seats.find('LL') != -1 else N)
+total_seats = int(input())
+
+seating_arrangement = input().rstrip()
+
+max_people = (len(seating_arrangement.replace('LL', 'L')) + 1 
+              if 'LL' in seating_arrangement else total_seats)
+print(max_people)
