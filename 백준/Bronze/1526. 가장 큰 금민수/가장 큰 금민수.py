f73fef6f3fe3ba07fb5eb42e5495f10243056b7d
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2024-05-03 11:01:51 +0900

    [Bronze I] Title: 가장 큰 금민수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1526. 가장 큰 금민수/가장 큰 금민수.py
+++ 백준/Bronze/1526. 가장 큰 금민수/가장 큰 금민수.py
@@ -2,25 +2,17 @@ import sys
 
 input = sys.stdin.readline
 
-def generate_lucky_numbers(limit):
-    lucky_numbers = []
-
-    def backtrack(number):
-        if number > limit:
-            return
-        if number > 0:
-            lucky_numbers.append(number)
-        backtrack(number * 10 + 4)
-        backtrack(number * 10 + 7)
-
-    backtrack(0)
-    return sorted(lucky_numbers)
-
-
-def find_largest_lucky_number(n):
-    lucky_numbers = generate_lucky_numbers(n)
-    return max(lucky_numbers) if lucky_numbers else -1
-
 N = int(input())
 
-print(find_largest_lucky_number(N))
+data = []
+
+def backtrack(data, num):
+    if num > N:
+        return
+    if num > 0:
+        data.append(num)
+    backtrack(data, num * 10 + 4)
+    backtrack(data, num * 10 + 7)
+
+backtrack(data, 0)
+print(max(data))
