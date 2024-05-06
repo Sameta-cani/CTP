4d6e14758d08e3c276415bdc87e62fc4d2cfbbea
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2024-05-06 17:32:45 +0900

    [Silver III] Title: 죽음의 게임, Time: 68 ms, Memory: 34008 KB -BaekjoonHub

---
+++ 백준/Silver/17204. 죽음의 게임/죽음의 게임.py
@@ -0,0 +1,23 @@
+from collections import deque
+import sys
+
+N, K = map(int, input().split())
+data = [int(input()) for _ in range(N)]
+visited = [False] * N
+
+queue = deque([0])
+
+ans = -1
+cnt = 0
+while queue:
+    now = queue.popleft()
+    if now == K:
+        ans = cnt
+        break
+    
+    if not visited[now]:
+        cnt += 1
+        visited[now] = True
+        queue.append(data[now])
+
+print(ans)
