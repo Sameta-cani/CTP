d07483f98e3f1a7e39108be945c55c73c99cd43c
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2024-05-19 18:33:50 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.02 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
@@ -2,7 +2,6 @@ from collections import defaultdict
 from math import ceil
 
 def solution(fees: list, records: list) -> list:
-    tmp = []
     default_time, default_fee = fees[0], fees[1]
     unit_time, unit_fee = fees[2], fees[3]
     
@@ -13,25 +12,25 @@ def solution(fees: list, records: list) -> list:
         time, number, state = record.split()
         if state == "IN":
             info[number] = time
+            if number not in fee_dict:
+                fee_dict[number] = 0
         elif state == "OUT":
-            # 요금 처리 알고리즘
             in_hour, in_min = map(int, info[number].split(':'))
             out_hour, out_min = map(int, time.split(':'))
             del_time = (out_hour * 60 + out_min) - (in_hour * 60 + in_min)
             fee_dict[number] += del_time
             del info[number]
     
-    if info:
-        for k, v in info.items():
-            in_hour, in_min = map(int, v.split(':'))
+    for k in fee_dict.keys():
+        if k in info:
+            in_hour, in_min = map(int, info[k].split(':'))
             del_time = (23 * 60 + 59) - (in_hour * 60 + in_min)
-            fee_dict[k] += del_time      
-    del info
-
-    for k, v in fee_dict.items():
-        fee_dict[k] = default_fee + ceil((v - default_time) / unit_time) * unit_fee if v > default_time else default_fee
-        tmp.append((k, fee_dict[k]))
+            fee_dict[k] += del_time
+            del info[k]
+            
+        total_time = fee_dict[k]
         
-    tmp.sort(key=lambda x: x[0])
-    answer = [v[1] for v in tmp]
+        fee_dict[k] = default_fee + ceil((total_time - default_time) / unit_time) * unit_fee if total_time > default_time else default_fee
+    
+    answer = [values[1] for values in sorted(fee_dict.items())]
     return answer
