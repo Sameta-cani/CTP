7ed3f0e9c3ea736ceda38fef0d0073584bad02c3
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2024-05-24 20:59:01 +0900

    [Bronze I] Title: 콘도 선정, Time: 52 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Bronze/2246. 콘도 선정/콘도 선정.py
+++ 백준/Bronze/2246. 콘도 선정/콘도 선정.py
@@ -1,13 +1,19 @@
-condos = []
-for i in range(int(input())):
-    l, p = map(int, input().split())
-    condos.append([l, p])
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+
+condos = [tuple(map(int, input().split())) for _ in range(N)]
 condos.sort()
-cost = 10001
-result = 0
-for i in condos:
-    temp = i[1]
-    if temp < cost:
-        cost = temp
-        result += 1
-print(result)
+
+cnt = 0
+min_cost = float('inf')
+
+for condo in condos:
+    cost = condo[1]
+    if cost < min_cost:
+        min_cost = cost
+        cnt += 1
+        
+print(cnt)
