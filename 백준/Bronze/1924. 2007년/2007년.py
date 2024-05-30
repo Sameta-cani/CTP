1be7de9aaf820a128ab24e8c1d36f0d3cd69e129
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2024-05-31 00:58:25 +0900

    [Bronze I] Title: 2007년, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1924. 2007년/2007년.py
+++ 백준/Bronze/1924. 2007년/2007년.py
@@ -8,17 +8,6 @@ x, y = map(int, input().split())
 
 base = (sum(day_of_month[1:x]) + y) % 7
 
-if base == 0:
-    print('SUN')
-elif base == 1:
-    print('MON')
-elif base == 2:
-    print('TUE')
-elif base == 3:
-    print('WED')
-elif base == 4:
-    print('THU')
-elif base == 5:
-    print('FRI')
-elif base == 6:
-    print('SAT')
+weekdays = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
+
+print(weekdays[base])
