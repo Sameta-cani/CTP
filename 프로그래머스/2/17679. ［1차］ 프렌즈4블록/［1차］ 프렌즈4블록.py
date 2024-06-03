0649ea5e642d27d1be5fa5be7f5cd9013d8c8986
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2024-06-04 02:01:44 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 2.46 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
@@ -1,8 +1,10 @@
 def updating(m: int, n: int, board: list) -> tuple:
+    # 2차원 리스트로 변환
     tmp = [list(row) for row in board]
     
     dx = [0, 1, 1]
     dy = [1, 0, 1]
+    # 지워질 블록의 위치를 담을 set
     target = set()
     
     for row in range(m-1):
@@ -10,12 +12,15 @@ def updating(m: int, n: int, board: list) -> tuple:
             base = tmp[row][col]
             if base == '-':
                 continue
+            # base 기준 4칸 모두 같으면 target에 해당 위치 전부 추가
             if all(tmp[row + x][col + y] == base for x, y in zip(dx, dy)):
                 target.update((row + x, col + y) for x ,y in zip([0] + dx, [0] + dy))
                 
+    # 타겟이 된 위치를 '-'로 변경
     for r, c in target:
         tmp[r][c] = '-'
         
+    # 각 열에 접근하여 '-'를 제거하고 문자를 아래로 이동
     for col in range(n):
         stack = [tmp[row][col] for row in range(m) if tmp[row][col] != '-']
         for row in range(m-1, -1, -1):
@@ -26,8 +31,10 @@ def updating(m: int, n: int, board: list) -> tuple:
 def solution(m: int, n: int, board: list) -> int:
     while True:
         board, target = updating(m, n, board)
+        # 업데이트 하면서 사라질 블록이 없으면 끝
         if not target:
             break
-        
+    
+    # 지워진 블록('-')의 개수 카운트
     res = sum(row.count('-') for row in board)
     return res
