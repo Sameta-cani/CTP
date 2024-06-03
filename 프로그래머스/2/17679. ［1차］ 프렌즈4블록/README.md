22502c7827ff6055d0beb9f51dbc2c8acfa9d1da
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2024-06-04 01:51:20 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 2.48 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/README.md
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/README.md
@@ -1,10 +1,10 @@
 # [level 2] [1차] 프렌즈4블록 - 17679 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17679) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17679?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 2.52 ms
+메모리: 10.2 MB, 시간: 2.48 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 04일 01:49:43
+2024년 06월 04일 01:51:19
 
 ### 문제 설명
 
--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
@@ -5,41 +5,33 @@ def updating(m: int, n: int, board: list):
     dy = [1, 0, 1]
     target = set()
     
+    # 2x2 블록의 일치를 검사합니다.
     for row in range(m-1):
         for col in range(n-1):
             base = tmp[row][col]
             if base == '-':
                 continue
-            is_same = all(tmp[row + x][col + y] == base for x, y in zip(dx, dy))
-            if is_same:
+            if all(tmp[row + x][col + y] == base for x, y in zip(dx, dy)):
                 target.update((row + x, col + y) for x, y in zip([0] + dx, [0] + dy))
                 
+    # 타겟이 된 위치를 '-'로 변경합니다.
     for r, c in target:
         tmp[r][c] = '-'
-            
+    
+    # 각 열을 처리하여 '-'를 제거하고 문자를 아래로 이동시킵니다.
     for col in range(n):
-        stack = []
-        for row in range(m):
-            if tmp[row][col] != '-':
-                stack.append(tmp[row][col])
-                    
+        stack = [tmp[row][col] for row in range(m) if tmp[row][col] != '-']
         for row in range(m-1, -1, -1):
-            if stack:
-                tmp[row][col] = stack.pop()
-            else:
-                tmp[row][col] = '-'
-                    
+            tmp[row][col] = stack.pop() if stack else '-'
+    
     return tmp, target
 
 def solution(m: int, n: int, board: list) -> int:
     while True:
         board, c = updating(m, n, board)
         if not c:
-            res = 0
-            for row in range(m):
-                for col in range(n):
-                    if board[row][col] == '-':
-                        res += 1
             break
     
+    # '-'의 개수를 셉니다.
+    res = sum(row.count('-') for row in board)
     return res
