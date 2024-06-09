15045d214b15045d1f917ef24a0edb073e5f7d8f
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2024-06-09 18:55:28 +0900

    [level 3] Title: 합승 택시 요금, Time: 288.22 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/README.md
+++ 프로그래머스/3/72413. 합승 택시 요금/README.md
@@ -1,10 +1,10 @@
 # [level 3] 합승 택시 요금 - 72413 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72413) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72413?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 288.96 ms
+메모리: 10.5 MB, 시간: 288.22 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 09일 18:45:57
+2024년 06월 09일 18:55:26
 
 ### 문제 설명
 
--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -1,20 +1,13 @@
 import sys
 
 input = sys.stdin.readline
-INF = int(1e9)
 
 def solution(n: int, s: int, a: int, b: int, fares: list) -> int:
-    answer = INF
-    graph = [[INF] * (n + 1) for _ in range(n + 1)]
+    INF = int(1e9)
     
-    for i in range(1, n + 1):
-        for j in range(1, n + 1):
-            if i == j:
-                graph[i][j] = 0
-                break
-            
-    for value in fares:
-        i, j, cost = value
+    graph = [[INF if i != j else 0 for j in range(n + 1)] for i in range(n + 1)]
+    
+    for i, j, cost in fares:
         graph[i][j] = cost
         graph[j][i] = cost
         
@@ -22,8 +15,11 @@ def solution(n: int, s: int, a: int, b: int, fares: list) -> int:
         for i in range(1, n + 1):
             for j in range(1, n + 1):
                 graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
-
+                
+    answer = INF
     for idx in range(1, n + 1):
-        answer = min(answer, graph[s][idx] + graph[idx][a] + graph[idx][b])
-        
+        if graph[s][idx] < INF and graph[idx][a] < INF and graph[idx][b] < INF:
+            total_cost = graph[s][idx] + graph[idx][a] + graph[idx][b]
+            answer = min(answer, total_cost)
+            
     return answer
