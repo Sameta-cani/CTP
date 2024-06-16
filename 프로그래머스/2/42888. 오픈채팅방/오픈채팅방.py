870622b542e7d453a90d13a93db4bd3654c27ce8
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2024-06-16 19:56:17 +0900

    [level 2] Title: 오픈채팅방, Time: 85.61 ms, Memory: 46.3 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
@@ -1,7 +1,4 @@
-import sys
 from collections import defaultdict
-
-input = sys.stdin.readline
 
 enter_format = "{}님이 들어왔습니다."
 leave_format = "{}님이 나갔습니다."
@@ -9,24 +6,19 @@ leave_format = "{}님이 나갔습니다."
 def solution(record: list) -> list:
     uid_info = defaultdict(str)
     actions = []
-    
+
     for data in record:
         command = data.split()
         action = command[0]
         uid = command[1]
-        
+
         if action in ("Enter", "Change"):
             name = command[2]
             uid_info[uid] = name
-            
+
         if action in ("Enter", "Leave"):
             actions.append((action, uid))
-            
-    answer = []
-    for action, uid in actions:
-        if action == "Enter":
-            answer.append(enter_format.format(uid_info.get(uid)))
-        else:
-            answer.append(leave_format.format(uid_info.get(uid)))
-            
+
+    answer = [enter_format.format(uid_info.get(uid)) if action == "Enter" else leave_format.format(uid_info.get(uid)) for action, uid in actions]
+
     return answer
