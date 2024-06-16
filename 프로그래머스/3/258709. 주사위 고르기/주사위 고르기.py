604be41c283eb27d2bf3ee3af6514cbc31a2b50a
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2024-06-16 19:53:43 +0900

    [level 3] Title: 주사위 고르기, Time: 1435.05 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.py
+++ 프로그래머스/3/258709. 주사위 고르기/주사위 고르기.py
@@ -1,7 +1,6 @@
 from itertools import combinations, product
 from collections import Counter
 
-
 def calculate_probabilities(dice: list) -> dict:
     num_dice = len(dice)
     possible_scores = Counter()
@@ -9,10 +8,10 @@ def calculate_probabilities(dice: list) -> dict:
     for rolls in product(*dice):
         score = sum(rolls)
         possible_scores[score] += 1
-    
+        
     total_outcomes = 6 ** num_dice
     probabilities = {score: count / total_outcomes for score, count in possible_scores.items()}
-    
+
     return probabilities
 
 def solution(dice: list) -> list:
@@ -20,10 +19,10 @@ def solution(dice: list) -> list:
     max_win_prob = 0
     
     for A_indices in combinations(range(len(dice)), n):
-        B_indices = [i for i in range(len(dice)) if i not in A_indices]
+        B_indices = [idx for idx in range(len(dice)) if idx not in A_indices]
         
-        A_dice = [dice[i] for i in A_indices]
-        B_dice = [dice[i] for i in B_indices]
+        A_dice = [dice[idx] for idx in A_indices]
+        B_dice = [dice[idx] for idx in B_indices]
         
         A_probs = calculate_probabilities(A_dice)
         B_probs = calculate_probabilities(B_dice)
@@ -33,10 +32,10 @@ def solution(dice: list) -> list:
             for b_score, b_prob in B_probs.items():
                 if a_score > b_score:
                     win_prob += a_prob * b_prob
-        
+                    
         if win_prob > max_win_prob:
             max_win_prob = win_prob
             best_combination = A_indices
             
-    ans = [val + 1 for val in best_combination]
+    ans = [idx + 1 for idx in best_combination]
     return ans
