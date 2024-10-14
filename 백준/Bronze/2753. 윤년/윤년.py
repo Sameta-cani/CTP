4c7c3c00e794a87f08a68b6f8793a21624ac13ec
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2024-10-14 22:07:28 +0900

    [Bronze V] Title: 윤년, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.py
+++ 백준/Bronze/2753. 윤년/윤년.py
@@ -1,8 +1,3 @@
-import sys
+year = int(input())
 
-year = int(sys.stdin.readline().rstrip())
-
-if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
-    print(1)
-else:
-    print(0)
+print(1 if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0 else 0)
