55dc34e456cf8bac1722cafac6c4d10fa6a9d6e7
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2024-10-14 22:21:20 +0900

    [Bronze V] Title: A+B - 3, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.py
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.py
@@ -1,7 +1,1 @@
-import sys
-
-T = int(sys.stdin.readline().rstrip())
-
-for _ in range(T):
-    A, B = map(int, sys.stdin.readline().rstrip().split())
-    print(A + B)
+for _ in[0]*int(input()):print(sum(map(int,input().split())))
