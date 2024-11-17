655501026a945489a3d7dca88f3d2485ac2486cf
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2024-11-17 21:22:42 +0900

    [Bronze I] Title: 팰린드롬수, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -1,13 +1,5 @@
-def is_palindrome(word):
-    is_palin = True
-    for i in range(len(word) // 2):
-        if word[i] != word[len(word) - (i + 1)]:
-            is_palin = False
-            break
-    return is_palin
-
-N = input()
-
-while N != '0':
-    print('yes' if is_palindrome(N) else 'no')
-    N = input()
+while True:
+    var = input()
+    if var == '0':
+        break
+    print("yes" if var == var[::-1] else "no")
