74816a2aa21c9bd7fe40d0ce06cf63c8018f1e86
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2024-11-17 21:05:46 +0900

    [Bronze II] Title: 블랙잭, Time: 108 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -3,10 +3,9 @@ from itertools import combinations
 N, M = map(int, input().split())
 cards = list(map(int, input().split()))
 
-max_sum = 0
-for comb in combinations(cards, 3):
-    temp_sum = sum(comb)
-    if temp_sum <= M:
-        max_sum = max(max_sum, temp_sum)
-
-print(max_sum)
+res = 0
+for iter in combinations(cards, 3):
+    if sum(iter) <= M:
+        res = max(res, sum(iter))
+        
+print(res)
