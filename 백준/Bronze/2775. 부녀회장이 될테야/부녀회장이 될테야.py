74a1ca7663452f852f19ee2fa700a9e11d02e5c8
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2024-11-19 19:34:24 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -1,16 +1,13 @@
-d = [[0] * 16 for _ in range(16)]
-d[1] = list(range(16))
-
-for i in range(2, 16):
+apt = [[0] * 16 for _ in range(16)]
+for i in range(1, 16):
+    apt[0][i] = i
+        
+for i in range(1, 16):
     for j in range(1, 16):
-        if j == 1:
-            d[i][j] = 1
-        else:
-            d[i][j] = d[i][j - 1] + d[i - 1][j]
+        apt[i][j] = apt[i][j-1] + apt[i-1][j]
 
-T = int(input())
-
-for _ in range(T):
+for _ in range(int(input())):
     k = int(input())
     n = int(input())
-    print(d[k + 1][n])
+    
+    print(apt[k][n])
