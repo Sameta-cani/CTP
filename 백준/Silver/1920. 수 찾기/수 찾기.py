3484417c10f265174136a6fd3e2a8e64c7f0c301
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2024-11-22 23:25:05 +0900

    [Silver IV] Title: 수 찾기, Time: 152 ms, Memory: 50520 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,10 +1,10 @@
 import sys
+input = sys.stdin.readline
 
-N = int(sys.stdin.readline().rstrip())
-n_array = set(map(int, sys.stdin.readline().rstrip().split()))
-
+N = int(input())
+A = set(map(int, input().split()))
 M = int(input())
-m_array = [1 if value in n_array else 0 for value in list(map(int, sys.stdin.readline().rstrip().split()))]
+arr = list(map(int, input().split()))
 
-for item in m_array:
-    sys.stdout.write(f'{item}\n')
+for num in arr:
+    print(1) if num in A else print(0)
