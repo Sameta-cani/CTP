0695edf5adcbee256451ac31cf7442d6cc71bf0c
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2024-11-22 22:46:43 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 60 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -1,16 +1,28 @@
+import sys
+input = sys.stdin.readline
+
 N, M = map(int, input().split())
-maps = [input() for _ in range(N)]
-counts = []
+boards = [input().strip() for _ in range(N)]
 
-for x in range(N - 7):
-    for y in range(M - 7):
-        for first_color in ['B', 'W']:
-            count = 0
-            for i in range(8):
-                for j in range(8):
-                    expected_color = first_color if (i + j) % 2 == 0 else 'B' if first_color == 'W' else 'W'
-                    if maps[x+i][y+j] != expected_color:
-                        count += 1
-            counts.append(count)
+def cal_repaints(x, y):
+    w_cnt, b_cnt = 0, 0
+    for i in range(x, x + 8):
+        for j in range(y, y + 8):
+            if (i + j) % 2 == 0:
+                if boards[i][j] != 'W':
+                    w_cnt += 1
+                if boards[i][j] != 'B':
+                    b_cnt += 1
+            else:
+                if boards[i][j] != 'B':
+                    w_cnt += 1
+                if boards[i][j] != 'W':
+                    b_cnt += 1
+    return min(w_cnt, b_cnt)
 
-print(min(counts))
+res = float('inf')
+for n in range(N - 7):
+    for m in range(M - 7):
+        res = min(res, cal_repaints(n, m))
+
+print(res)
