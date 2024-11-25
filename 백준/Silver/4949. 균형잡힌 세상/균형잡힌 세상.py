480a9d47d9062f0a92fc2e206868ce0a217f262f
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2024-11-26 02:58:56 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 80 ms, Memory: 34028 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -6,19 +6,24 @@ while True:
     sen = input().rstrip()
     if sen == '.':
         break
-    stack = deque(list())
+    stack = deque()
+    is_balanced = True
     for ch in sen:
-        if ch in ['(', '[']:
+        if ch in '([':
             stack.append(ch)
         elif ch == ')':
             if stack and stack[-1] == '(':
                 stack.pop()
             else:
-                stack.append(ch)
+                is_balanced = False
+                break
         elif ch == ']':
             if stack and stack[-1] == '[':
                 stack.pop()
             else:
-                stack.append(ch)
+                is_balanced = False
+                break
         
-    print('no' if stack else 'yes')
+    if stack:
+        is_balanced = False
+    print('yes' if is_balanced else 'no')
