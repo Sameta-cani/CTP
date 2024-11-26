ddcb93e76957901da1c16a7982af7f1b3cf89387
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2024-11-27 02:42:13 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 952 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.py
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.py
@@ -1,11 +1,5 @@
 import sys
+input = sys.stdin.readline
 
-# 테스트 케이스의 개수 T 입력
-T = int(sys.stdin.readline().rstrip())
-
-# T만큼 입력
-for i in range(T):
-  # 두 정수 A와 B 입력
-  A, B = map(int, sys.stdin.readline().split())
-  # 합 출력
-  sys.stdout.write(str(A+B)+'\n')
+for _ in range(int(input())):
+    sys.stdout.write(str(sum(map(int, input().split())))+'\n')
