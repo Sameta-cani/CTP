ba5085f1d919bf44e64152bac1af390db58d57bd
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2024-11-28 00:54:15 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 52 ms, Memory: 34008 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -1,13 +1,12 @@
 from collections import deque
 
 N, K = map(int, input().split())
-
 queue = deque(range(1, N + 1))
 
-elimination_order = []
+del_order = []
 
 while queue:
     queue.rotate(-(K - 1))
-    elimination_order.append(str(queue.popleft()))
+    del_order.append(str(queue.popleft()))
 
-print("<" + ", ".join(elimination_order) + ">")
+print("<" + ", ".join(del_order) + ">")
