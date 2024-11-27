cb9fce6210bba22d1360b49cca518fd414b54fad
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2024-11-27 16:07:40 +0900

    [Silver IV] Title: 괄호, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,15 +1,18 @@
-for tc in range(int(input())):
-    data = input()
+for _ in range(int(input())):
     stack = []
-    is_VPS = True
-
-    for s in data:
-        if s == '(':
-            stack.append(s)
-        else:
-            if not stack:
-                is_VPS = False
+    is_vps = True
+    ps = input().strip()
+    for p in ps:
+        if p == '(':
+            stack.append(p)
+        elif p == ')':
+            if not stack or stack[-1] == ')':
+                is_vps = False
                 break
-            stack.pop()
+            elif stack[-1] == '(':
+                stack.pop()
 
-    print('YES' if is_VPS and not stack else 'NO')
+    if stack:
+        is_vps = False
+    
+    print('YES' if is_vps else 'NO')
