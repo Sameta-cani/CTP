e51cc34ea1eb5580383c363570900626aec7f56f
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2024-11-29 01:44:40 +0900

    [Silver III] Title: 소수 구하기, Time: 208 ms, Memory: 43808 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,14 +1,11 @@
-def prime_list(m, n):
-    is_prime = [True] * (n + 1)
-    is_prime[0], is_prime[1] = False, False
-
-    for i in range(2, int(n**0.5) + 1):
-        if is_prime[i]:
-            for j in range(i * i, n + 1, i):
-                is_prime[j] = False
-
-    return [i for i in range(m, n + 1) if is_prime[i]]
-
 M, N = map(int, input().split())
 
-print(*prime_list(M, N), sep='\n')
+is_prime = [True] * (N + 1)
+is_prime[0] = is_prime[1] = False
+
+for i in range(2, int(N**0.5) + 1):
+    if is_prime[i]:
+        for j in range(i*i, N + 1, i):
+            is_prime[j] = False
+
+print('\n'.join(map(str, (val for val in range(M, N + 1) if is_prime[val]))))
