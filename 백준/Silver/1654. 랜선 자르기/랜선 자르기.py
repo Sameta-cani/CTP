9f4067598e217d2e129b07485c70d94728244ffb
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2024-11-30 04:45:21 +0900

    [Silver II] Title: 랜선 자르기, Time: 304 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -1,19 +1,15 @@
-import sys
-input = sys.stdin.readline
-
 K, N = map(int, input().split())
-array = [int(input().rstrip()) for _ in range(K)]
+data = [int(input()) for _ in range(K)]
 
 start = 1
-end = max(array)
-
+end = max(data)
 result = 0
+
 while start <= end:
-    total = 0
     mid = (start + end) // 2
-    for val in array:
-        total += val // mid
-    if total < N:
+    cnt = sum(val // mid for val in data)
+    
+    if cnt < N:
         end = mid - 1
     else:
         result = mid
