5dc8d0c130909d20de90b6aefc2209b79c5296de
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2024-12-01 21:37:17 +0900

    [Silver V] Title: 집합, Time: 3408 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -1,24 +1,46 @@
+import sys
 input = sys.stdin.readline
 
-S = set()
+class CustomSet:
+    def __init__(self):
+        self.obj = set()
+
+    def add(self, element):
+        self.obj.add(element)
+
+    def remove(self, element):
+        self.obj.discard(element)
+    
+    def toggle(self, element):
+        if element in self.obj:
+            self.obj.remove(element)
+        else:
+            self.obj.add(element)
+
+    def check(self, element):
+        print(1 if element in self.obj else 0)
+    
+    def all(self):
+        self.obj = set(range(1, 21))
+
+    def empty(self):
+        self.obj.clear()
+
+S = CustomSet()
 
 for _ in range(int(input())):
-    command = input().split()
-    prompt = command[0]
+    command = input().strip().split()
+    cmd = command[0]
 
-    if prompt == 'add':
-        value = int(command[1])
-        S.add(value)
-    elif prompt == 'remove':
-        value = int(command[1])
-        if value in S:
-            S.remove(value)
-    elif prompt == 'check':
-        print(1 if int(command[1]) in S else 0)
-    elif prompt == 'toggle':
-        value = int(command[1])
-        S.remove(value) if value in S else S.add(value)
-    elif prompt == 'all':
-        S = set(range(1, 21))
-    elif prompt == 'empty':
-        S = set()
+    if cmd == 'add':
+        S.add(int(command[1]))
+    elif cmd == 'remove':
+        S.remove(int(command[1]))
+    elif cmd == 'check':
+        S.check(int(command[1]))
+    elif cmd == 'toggle':
+        S.toggle(int(command[1]))
+    elif cmd == 'all':
+        S.all()
+    elif cmd == 'empty':
+        S.empty()
