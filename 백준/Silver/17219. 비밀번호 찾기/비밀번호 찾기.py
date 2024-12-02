4ba4c8faa8a99c853146154ff656381486a12bda
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2024-12-02 19:37:02 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 152 ms, Memory: 51208 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
@@ -1,14 +1,7 @@
-from collections import defaultdict
 import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
+log_info = {site: pw for site, pw in (input().strip().split() for _ in range(N))}
 
-site_dict = defaultdict(str)
-for _ in range(N):
-    site_info = input().rstrip().split()
-    site_dict[site_info[0]] = site_info[1]    
-
-targets = [site_dict.get(input().strip()) for _ in range(M)]
-
-print(*targets, sep='\n')
+sys.stdout.write('\n'.join(log_info[input().strip()] for _ in range(M)) + '\n')
