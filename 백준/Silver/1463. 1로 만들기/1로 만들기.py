9a9e6399a252f399284a1f7f2f29432ff0b99de6
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2024-12-03 22:02:24 +0900

    [Silver III] Title: 1로 만들기, Time: 520 ms, Memory: 38932 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,12 +1,11 @@
 N = int(input())
+dp = [0] * (N + 1)
 
-d = [0] * (N + 1)
+for idx in range(2, N + 1):
+    dp[idx] = dp[idx - 1] + 1
+    if idx % 2 == 0:
+        dp[idx] = min(dp[idx], dp[idx // 2] + 1)
+    if idx % 3 == 0:
+        dp[idx] = min(dp[idx], dp[idx // 3] + 1)
 
-for i in range(2, N + 1):
-    d[i] = d[i - 1] + 1  # 현재 수에서 1을 빼는 경우
-    if i % 2 == 0:  # 현재 수가 2로 나누어 떨어지는 경우
-        d[i] = min(d[i], d[i // 2] + 1)
-    if i % 3 == 0:  # 현재 수가 3으로 나누어 떨어지는 경우
-        d[i] = min(d[i], d[i // 3] + 1)
-
-print(d[N])
+print(dp[N])
