55e148a164cdd7a352a8b84f0a3f7a9f99492cac
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2025-01-14 00:07:46 +0900

    [Bronze III] Title: 첨탑 밀어서 부수기, Time: 1996 ms, Memory: 544200 KB -BaekjoonHub

--- 백준/Bronze/28014. 첨탑 밀어서 부수기/첨탑 밀어서 부수기.py
+++ 백준/Bronze/28014. 첨탑 밀어서 부수기/첨탑 밀어서 부수기.py
@@ -1,13 +1,9 @@
-import sys
-
-input = sys.stdin.readline
-
 N = int(input())
-array = list(map(int, input().split()))
+heights = list(map(int, input().split()))
+cnt = 1
 
-count = 1
-for index in range(N - 1):
-    if array[index] <= array[index + 1]:
-        count += 1
-
-print(count)
+for idx in range(1, N):
+    if heights[idx - 1] <= heights[idx]:
+        cnt += 1
+        
+print(cnt)
