10ad274d600496ed84140039f041641edb13a5d9
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2025-01-13 22:47:07 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 60 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -1,13 +1,8 @@
-import sys
-
-input = sys.stdin.readline
-
-coins = [25, 10, 5, 1]
+coin_types = [25, 10, 5, 1]
 
 for _ in range(int(input())):
     C = int(input())
-
-    for coin in coins:
+    for coin in coin_types:
         print(C // coin, end=' ')
         C %= coin
     print()
