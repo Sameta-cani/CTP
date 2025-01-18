776d6df01ed3563bf8f12481dd8abfd7441302be
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2025-01-18 17:46:11 +0900

    [Bronze V] Title: Pyramids, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5341. Pyramids/Pyramids.py
+++ 백준/Bronze/5341. Pyramids/Pyramids.py
@@ -1,11 +1,6 @@
-import sys
-
-input = sys.stdin.readline
-
 while True:
     n = int(input())
-
     if n == 0:
         break
-
-    print(sum(list(range(1, n + 1))))
+    
+    print(sum(range(n + 1)))
