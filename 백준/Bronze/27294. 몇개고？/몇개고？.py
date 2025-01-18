17307c08914cacaca0dde90c130557ae7278b084
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2025-01-18 17:12:13 +0900

    [Bronze V] Title: 몇개고?, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/27294. 몇개고？/몇개고？.py
+++ 백준/Bronze/27294. 몇개고？/몇개고？.py
@@ -1,9 +1,3 @@
 T, S = map(int, input().split())
 
-# 점심 시간이고 술을 먹지 않는 경우
-mask = (12 <= T <= 16) and (S == 0)
-
-if mask:
-    print(320)
-else:
-    print(280)
+print(320 if (12 <= T <= 16) and (S == 0) else 280)
