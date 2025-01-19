c5a111a471b8bf7f1c88ab214dd35d99527e7489
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2025-01-19 18:43:46 +0900

    [Bronze IV] Title: 전자레인지, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/14470. 전자레인지/전자레인지.py
@@ -0,0 +1,14 @@
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+E = int(input())
+
+ans = 0
+if A < 0:
+    ans += abs(A) * C + D
+    A = 0
+
+ans += (B - A) * E
+    
+print(ans)
