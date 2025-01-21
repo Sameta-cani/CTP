a69c2b01350b50c8691971c5a31470b073334249
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2025-01-21 19:38:59 +0900

    [Silver III] Title: 바이러스, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,29 +1,27 @@
-from collections import defaultdict
 import sys
+
 input = sys.stdin.readline
 
 N = int(input())
 M = int(input())
+graph = [[] for _ in range(N + 1)]
+visited = [False] * (N + 1)
 
-graph = defaultdict(list)
 for _ in range(M):
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
-
-def dfs(start):
-    stack = [start]
-    visited = [False] * (N + 1)
-    visited[start] = True
-
-    while stack:
-        current = stack.pop()
-        for neighbor in graph[current]:
-            if not visited[neighbor]:
-                visited[neighbor] = True
-                stack.append(neighbor)
-    return visited
-
-visited = dfs(1)
-
-print(sum(visited) - 1)
+    
+    
+cnt = -1
+stack = [1]
+while stack:
+    v = stack.pop()
+    
+    if not visited[v]:
+        visited[v] = True
+        cnt += 1
+        
+        stack.extend(node for node in graph[v] if not visited[node])
+        
+print(cnt)
