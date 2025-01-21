5341049184ae296c0d63d882642983232e43295d
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2025-01-21 09:54:32 +0900

    [Silver IV] Title: 점프왕 쩰리 (Small), Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/16173. 점프왕 쩰리 （Small）/점프왕 쩰리 （Small）.py
+++ 백준/Silver/16173. 점프왕 쩰리 （Small）/점프왕 쩰리 （Small）.py
@@ -6,22 +6,23 @@ N = int(input())
 graph = [list(map(int, input().split())) for _ in range(N)]
 visited = [[False] * N for _ in range(N)]
 
-stack = [(0, 0)]
+cond = False
 
-is_cond = False
+stack = [(0, 0)]
 while stack:
-    cx, cy = stack.pop()
-    dist = graph[cx][cy]
+    x, y = stack.pop()
+    dist = graph[x][y]
     
     if dist == -1:
-        is_cond = True
+        cond = True
         break
     
-    if not visited[cx][cy]:
-        visited[cx][cy] = True
-        for dx, dy in [(0, 1), (1, 0)]:
-            nx, ny = cx + dist * dx, cy + dist * dy
+    if not visited[x][y]:
+        visited[x][y] = True
+        
+        for dx, dy in [(1, 0), (0, 1)]:
+            nx, ny = x + dist * dx, y + dist * dy
             if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny]:
                 stack.append((nx, ny))
-            
-print("HaruHaru" if is_cond else "Hing")
+                
+print("HaruHaru" if cond else "Hing")
