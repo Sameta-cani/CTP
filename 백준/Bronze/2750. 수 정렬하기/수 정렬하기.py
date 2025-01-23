42df2d2906fa01b5e8e13bbb8d3ecbc4416b3cec
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2025-01-23 14:48:52 +0900

    [Bronze II] Title: 수 정렬하기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,31 +1,7 @@
 import sys
-sys.setrecursionlimit(10**6)
 
-# quick sort
-N = int(input())
-data = [int(input()) for _ in range(N)]
+input = sys.stdin.readline
 
-def quick_sort(data, start, end):
-    if start >= end: # 원소가 1개인 경우 종료
-        return
-    pivot = start # 피벗은 첫 번째 원소
-    left = start + 1
-    right = end
-    while left <= right:
-        # 피벗보다 큰 데이터를 찾을 때까지 반복
-        while left <= end and data[left] <= data[pivot]:
-            left += 1
-        # 피벗보다 작은 데이터를 찾을 때까지 반복
-        while right > start and data[right] >= data[pivot]:
-            right -= 1
-        if left > right: # 엇갈렸다면 작은 데이터와 피벗을 교체
-            data[right], data[pivot] = data[pivot], data[right]
-        else: # 엇갈리지 않았다면 작은 데이터와 큰 데이터를 교체
-            data[left], data[right] = data[right], data[left]
-    # 분할 이후 왼쪽 부분과 오른쪽 부분에서 각각 정렬 수행
-    quick_sort(data, start, right - 1)
-    quick_sort(data, right + 1, end)
+data = sorted([int(input()) for _ in range(int(input()))])
 
-quick_sort(data, 0, len(data) - 1)
-
-print(*data, sep='\n')
+print('\n'.join(map(str, data)))
