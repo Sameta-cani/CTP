e2cac294b73f58508e3f836dbdb0ef77868d0e7e
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2025-01-23 23:24:54 +0900

    [Silver V] Title: 숫자 카드, Time: 2032 ms, Memory: 111132 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,12 +1,27 @@
 import sys
 
-N = int(sys.stdin.readline().rstrip())
-A = set(map(int, sys.stdin.readline().rstrip().split()))
-M = int(sys.stdin.readline().rstrip())
-B = list(map(int, sys.stdin.readline().rstrip().split()))
+input = sys.stdin.readline
 
-for card in B:
-    if card in A:
+def binary_saerch(array, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        if array[mid] == target:
+            return mid
+        elif array[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return None
+
+N = int(input())
+N_array = list(map(int, input().split()))
+N_array.sort()
+M = int(input())
+M_array = list(map(int, input().split()))
+
+for item in M_array:
+    result = binary_saerch(N_array, item, 0, N - 1)
+    if result != None:
         print(1, end=' ')
     else:
         print(0, end=' ')
