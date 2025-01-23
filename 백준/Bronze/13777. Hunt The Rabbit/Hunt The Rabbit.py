4207f060765a44550cec4b9390c8ba79b94e24e4
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2025-01-23 23:08:37 +0900

    [Bronze I] Title: Hunt The Rabbit, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/13777. Hunt The Rabbit/Hunt The Rabbit.py
+++ 백준/Bronze/13777. Hunt The Rabbit/Hunt The Rabbit.py
@@ -1,21 +1,20 @@
-def binary_search(array, target, start, end):
-    result = []
+def binary_search(target):
+    start = 1
+    end = 50
     while start <= end:
         mid = (start + end) // 2
-        if array[mid] == target:
-            result.append(mid+1)
-            return result
-        elif array[mid] > target:
+        print(mid, end=' ')
+        if mid == target:
+            break
+        elif mid > target:
             end = mid - 1
         else:
             start = mid + 1
-        result.append(mid+1)
-    return result
 
-array = list(range(1, 51))
-N = int(input())
-
-while N != 0:
-    result = binary_search(array, N, 0, len(array)-1)
-    print(*result)
-    N = int(input())
+while True:
+    n = int(input())
+    if n == 0:
+        break
+    
+    binary_search(n)
+    print()
