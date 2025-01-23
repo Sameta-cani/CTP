1b711aa0758cf1ac8c29e0ae24f289f6ce750661
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2025-01-23 14:53:31 +0900

    [Bronze II] Title: 대표값2, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2587. 대표값2/대표값2.py
+++ 백준/Bronze/2587. 대표값2/대표값2.py
@@ -1,25 +1,8 @@
-array = [int(input()) for _ in range(5)]
+import sys
 
-def quick_sort(array, start, end):
-    if start >= end:
-        return
-    pivot = start
-    left = pivot + 1
-    right = end
-    while left <= right:
-        while left <= end and array[left] <= array[pivot]:
-            left += 1
-        while right > start and array[right] >= array[pivot]:
-            right -= 1
-        if left > right:
-            array[right], array[pivot] = array[pivot], array[right]
-        else:
-            array[left], array[right] = array[right], array[left]
-        
-    quick_sort(array, start, right - 1)
-    quick_sort(array, right + 1, end)
+input = sys.stdin.readline
 
-quick_sort(array, 0, len(array) - 1)
+data = sorted([int(input()) for _ in range(5)])
 
-print(sum(array) // len(array))
-print(array[len(array) // 2])
+print(sum(data) // 5)
+print(data[2])
