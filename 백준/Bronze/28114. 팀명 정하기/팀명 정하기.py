9238b1cd296cd00ea9005ba47ced9d824689b406
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2025-01-23 16:34:57 +0900

    [Bronze II] Title: 팀명 정하기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/28114. 팀명 정하기/팀명 정하기.py
+++ 백준/Bronze/28114. 팀명 정하기/팀명 정하기.py
@@ -1,13 +1,11 @@
-# 사용자로부터 3개의 데이터 조각 입력 받기
-data = [tuple(input().split()) for _ in range(3)]
+import sys
 
-# 연도 데이터 추출 및 정렬
-years = sorted([int(year) % 100 for _, year, _ in data])
+input = sys.stdin.readline
 
-# 이름 데이터 추출 및 정렬
-names_sorted = sorted(data, key=lambda x: int(x[0]), reverse=True)
-names = [name[0] for _, _, name in names_sorted]
+data = [input().strip().split() for _ in range(3)]
 
-# 결과 출력
-print("".join(map(str, years)))
-print("".join(names))
+sorted_by_second = sorted(data, key=lambda x: int(x[1]))
+print(''.join(str(int(row[1]) % 100) for row in sorted_by_second))
+
+sorted_by_first_dec = sorted(data, key=lambda x: int(x[0]), reverse=True)
+print(''.join(row[2][0] for row in sorted_by_first_dec))
