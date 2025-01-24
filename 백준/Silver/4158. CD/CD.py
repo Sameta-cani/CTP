d6c87a6b546974828b0bcfbed87d31a27789575b
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2025-01-24 14:40:15 +0900

    [Silver V] Title: CD, Time: 2040 ms, Memory: 156460 KB -BaekjoonHub

--- 백준/Silver/4158. CD/CD.py
+++ 백준/Silver/4158. CD/CD.py
@@ -2,13 +2,36 @@ import sys
 
 input = sys.stdin.readline
 
-def count_common_elements(n, m):
-    set_n = {int(input().rstrip()) for _ in range(n)}
-    set_m = {int(input().rstrip()) for _ in range(m)}
-    return len(set_n & set_m)
+def binary_search(array, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        if array[mid] == target:
+            return mid
+        elif array[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return None
 
 while True:
-    N, M = map(int, input().rstrip().split())
+    N, M = map(int, input().split())
+    
     if N == 0 and M == 0:
         break
-    print(count_common_elements(N, M))
+    
+    N_array = list(int(input()) for _ in range(N))
+    M_array = list(int(input()) for _ in range(M))
+    
+    ptr1, ptr2 = 0, 0
+    cnt = 0
+    while ptr1 < N and ptr2 < M:
+        if N_array[ptr1] < M_array[ptr2]:
+            ptr1 += 1
+        elif N_array[ptr1] > M_array[ptr2]:
+            ptr2 += 1
+        else:
+            cnt += 1
+            ptr1 += 1
+            ptr2 += 1
+            
+    print(cnt)
