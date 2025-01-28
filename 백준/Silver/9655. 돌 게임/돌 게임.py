498f52c0cce28aec20b481b02854d2f2b7bf8eff
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2025-01-29 03:21:39 +0900

    [Silver V] Title: 돌 게임, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.py
+++ 백준/Silver/9655. 돌 게임/돌 게임.py
@@ -1,3 +1,17 @@
-N = int(input())
+N = int(input())  # 돌의 개수
 
-print('SK' if N % 2 != 0 else 'CY')
+# DP 배열 초기화
+dp = [0] * (N + 1)
+dp[1] = 1
+if N > 1:
+    dp[2] = 2
+
+# DP 점화식 계산
+for i in range(3, N + 1):
+    dp[i] = min(dp[i - 1] + 1, dp[i - 3] + 1)
+
+# 게임 과정 횟수의 홀짝에 따라 승패 결정
+if dp[N] % 2 == 1:
+    print("SK")
+else:
+    print("CY")
