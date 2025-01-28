e37411848d6ae760a96822c3f31275c932cbef5f
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2025-01-28 21:06:18 +0900

    [Bronze II] Title: 공 바꾸기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -1,10 +1,8 @@
-import sys
+N, M = map(int, input().split())
 
-N, M = map(int, sys.stdin.readline().rstrip().split())
-baskets = list(range(1, N+1))
-
+array = list(range(N + 1))
 for _ in range(M):
-    i, j = map(int, sys.stdin.readline().rstrip().split())
-    baskets[i-1], baskets[j-1] = baskets[j-1], baskets[i-1]
-
-print(*baskets)
+    i, j = map(int, input().split())
+    array[i], array[j] = array[j], array[i]
+    
+print(*array[1:])
