bc4da585f337ac2dde5cabfb97bcc3a5157ee4b1
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2025-01-28 21:03:52 +0900

    [Bronze III] Title: 공 넣기, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.py
+++ 백준/Bronze/10810. 공 넣기/공 넣기.py
@@ -1,10 +1,9 @@
-import sys
+N, M = map(int, input().split())
 
-N, M = map(int, sys.stdin.readline().rstrip().split())
-baskets = [0] * N
-
+array = [0] * (N + 1)
 for _ in range(M):
-    i, j, k = map(int, sys.stdin.readline().rstrip().split())
-    baskets[i-1:j] = [k] * (j - i + 1)
-
-print(*baskets)
+    i, j, k = map(int, input().split())
+    for idx in range(i, j + 1):
+        array[idx] = k
+    
+print(*array[1:])
