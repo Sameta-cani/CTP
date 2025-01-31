993d76bf68e06d4dfbd01b397513b7c6f56d5e2a
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2025-01-31 20:12:07 +0900

    [Silver II] Title: 친구, Time: 80 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1058. 친구/친구.py
+++ 백준/Silver/1058. 친구/친구.py
@@ -6,26 +6,26 @@ INF = int(1e9)
 N = int(input())
 graph = [[INF] * (N + 1) for _ in range(N + 1)]
 
-for a in range(1, N + 1):
-    data = input()
-    for b in range(1, N + 1):
-        if a == b:
-            graph[a][b] = 0
-        else:
-            if data[b-1] == 'Y':
-                graph[a][b] = 1
-
+for i in range(1, N + 1):
+    info = input().strip()
+    for j in range(1, N + 1):
+        if i == j:
+            graph[i][j] = 0
+        if info[j - 1] == 'Y':
+            graph[i][j] = 1
+            
 for k in range(1, N + 1):
-    for a in range(1, N + 1):
-        for b in range(1, N + 1):
-            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
-
-max_count = -INF
-for a in range(1, N + 1):
-    count = 0
-    for b in range(1, N + 1):
-        if a != b and 0 < graph[a][b] <= 2:
-            count += 1
-    max_count = max(max_count, count)
-
-print(max_count)
+    for i in range(1, N + 1):
+        for j in range(1, N + 1):
+            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+    
+    
+max_value = -1            
+for row in range(1, N + 1):
+    cnt = 0
+    for col in range(1, N + 1):
+        if 1 <= graph[row][col] <= 2:
+            cnt += 1
+    max_value = max(max_value, cnt)
+    
+print(max_value)
