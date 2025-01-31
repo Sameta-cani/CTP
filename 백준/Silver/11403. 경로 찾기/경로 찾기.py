e73d0bad07e147c76fb24b6ab91fdd3a29659395
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2025-01-31 23:56:13 +0900

    [Silver I] Title: 경로 찾기, Time: 340 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.py
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -1,15 +1,26 @@
 import sys
+
 input = sys.stdin.readline
+INF = int(1e9)
 
 N = int(input())
+graph = [[INF] * (N + 1) for _ in range(N + 1)]
 
-graph = [list(map(int, input().split())) for _ in range(N)]
-
-for k in range(N):
-    for a in range(N):
-        for b in range(N):
-            if graph[a][k] and graph[k][b]:
-                graph[a][b] = 1
-
-for row in graph:
-    print(*row)
+for i in range(1, N + 1):
+    data = list(map(int, input().split()))
+    for j, val in enumerate(data, start=1):
+        if val == 1:
+            graph[i][j] = 1
+            
+for k in range(1, N + 1):
+    for i in range(1, N + 1):
+        for j in range(1, N + 1):
+            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+            
+for row in range(1, N + 1):
+    for col in range(1, N + 1):
+        if graph[row][col] == INF:
+            print(0, end=' ')
+        else:
+            print(1, end=' ')
+    print()
