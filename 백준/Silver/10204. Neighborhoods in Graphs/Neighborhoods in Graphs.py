a2a2ef6ff36b7f6c6450691b78c5a544679372a9
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2025-01-31 23:35:38 +0900

    [Silver II] Title: Neighborhoods in Graphs, Time: 36 ms, Memory: 35508 KB -BaekjoonHub

--- 백준/Silver/10204. Neighborhoods in Graphs/Neighborhoods in Graphs.py
+++ 백준/Silver/10204. Neighborhoods in Graphs/Neighborhoods in Graphs.py
@@ -4,7 +4,7 @@ import sys
 input = sys.stdin.readline
 INF = int(1e9)
 
-def dijkstra(start: int, graph: list) -> list:
+def dijkstra(start: int, graph: list, N: int) -> list:
     distance = [INF] * (N + 1)
     priority_queue = []
     heapq.heappush(priority_queue, (0, start))
@@ -17,11 +17,11 @@ def dijkstra(start: int, graph: list) -> list:
             continue
         
         for adj, weight in graph[current_node]:
-            distance_via_current = current_distance + weight
+            new_distance = current_distance + weight
             
-            if distance_via_current < distance[adj]:
-                distance[adj] = distance_via_current
-                heapq.heappush(priority_queue, (distance_via_current, adj))
+            if new_distance < distance[adj]:
+                distance[adj] = new_distance
+                heapq.heappush(priority_queue, (new_distance, adj))
                 
     return distance
 
@@ -29,22 +29,18 @@ for _ in range(int(input())):
     data = input().strip().split()
     N, E = map(int, data[:2])
     graph = [[] for _ in range(N + 1)]
-    edges_info = data[2:2+2*E]
+    edges_info = data[2:2 + 2 * E]
 
     for idx in range(0, len(edges_info), 2):
-        edge = edges_info[idx:idx+2]
-        u, v = int(edge[0][1]), int(edge[1][1])
+        u, v = int(edges_info[idx][1]), int(edges_info[idx + 1][1])
         graph[u].append((v, 1))
         graph[v].append((u, 1))
     
     
     start = int(data[-1][1])
-    distance = dijkstra(start, graph)
     
-    cnt = 0
-    for dist in distance:
-        if 1 <= dist <= 2:
-            cnt += 1
+    distance = dijkstra(start, graph, N)
+    
+    cnt = sum(1 for d in distance if 1 <= d <= 2)
             
     print(f"The number of supervillains in 2-hop neighborhood of {data[-1]} is {cnt}")
-    
