fe16789202ac6b661ec7f3711e6ffc0b4880f818
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2025-01-31 19:27:00 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 2800 ms, Memory: 173372 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -1,40 +1,37 @@
-from collections import deque
+import heapq
 import sys
 
 input = sys.stdin.readline
+INF = int(1e9)
 
-# 도시의 개수, 도로의 개수, 거리 정보, 출발 도시 번호
+def dijkstra(start: int, graph: list, distance: list) -> None:
+    priority_queue = []
+    heapq.heappush(priority_queue, (0, start))
+    distance[start] = 0
+    
+    while priority_queue:
+        current_distance, current_node = heapq.heappop(priority_queue)
+        
+        if distance[current_node] < current_distance:
+            continue
+        
+        for adj, weight in graph[current_node]:
+            distance_via_current = current_distance + weight
+            if distance_via_current < distance[adj]:
+                distance[adj] = distance_via_current
+                heapq.heappush(priority_queue, (distance_via_current, adj))
+
 N, M, K, X = map(int, input().split())
 graph = [[] for _ in range(N + 1)]
+distance = [INF] * (N + 1)
 
-# 모든 도로 정보 입력받기
 for _ in range(M):
-    a, b = map(int, input().split())
-    graph[a].append(b)
+    u, v = map(int, input().split())
+    graph[u].append((v, 1))
+    
+dijkstra(X, graph, distance)
 
-# 모든 도시에 대한 최단 거리 초기화
-distance = [-1] * (N + 1)
-distance[X] = 0 # 출발 도시까지의 거리는 0으로 설정
+k_distances = [idx for idx in range(1, N + 1) if distance[idx] == K]
 
-# 너비 우선 탐색(BFS) 수행
-q = deque([X])
-while q:
-    now = q.popleft()
-    # 현재 도시에서 이동할 수 있는 모든 도시를 확인
-    for next_node in graph[now]:
-        # 아직 방문하지 않은 도시라면
-        if distance[next_node] == -1:
-            # 최단 거리 갱신
-            distance[next_node] = distance[now] + 1
-            q.append(next_node)
-
-# 최단 거리가 K인 모든 도시의 번호를 오름차순으로 출력
-check = False
-for idx in range(1, N + 1):
-    if distance[idx] == K:
-        print(idx)
-        check = True
-
-# 만약 최단 거리가 K인 도시가 없다면, -1 출력
-if not check:
-    print(-1)
+print('\n'.join(map(str, k_distances)) if k_distances else -1)
+    
