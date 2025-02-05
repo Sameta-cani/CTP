9160aa6ee240b5a9bd6aabbd0f796c57ac703453
ParkSangJin <83288284+Sameta-cani@users.noreply.github.com>
2025-02-05 19:36:22 +0900

    [Gold III] Title: 크게 만들기, Time: 164 ms, Memory: 40620 KB -BaekjoonHub

---
+++ 백준/Gold/2812. 크게 만들기/크게 만들기.py
@@ -0,0 +1,12 @@
+N, K = map(int, input().split())
+number = list(input())
+
+answer = []
+cnt = K
+for num in number:
+    while answer and cnt > 0 and answer[-1] < num:
+        del answer[-1]
+        cnt -= 1
+    answer.append(num)
+    
+print(''.join(answer[:N-K]))
